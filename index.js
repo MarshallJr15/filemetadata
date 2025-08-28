d0fa9db3d2a44d0000137b34bd11cde8be8e7323
Marshall Murdock <marshallmurdock19@gmail.com>
2025-08-28 16:43:57 +0000

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const mutler = require('multer')
+const upload = mutler({ dest: './uploads'})
 
 var app = express();
 
@@ -11,7 +13,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const {originalname, mimetype, size} = req.file
+  console.log(req.file)
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size
+  })
+})
 
 
 const port = process.env.PORT || 3000;
